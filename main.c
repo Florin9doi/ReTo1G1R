14fd12a89090e32a30362d5ad12e6a006bdd1a7d
Florin9doi <Florin9doi@users.noreply.github.com>
2020-04-11 14:49:35 +0300

    speedup 10 times

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 #define REGIONS "Asia|Australia|Austria|Belgium|Brazil|Canada|China|Croatia|Denmark|Europe|Finland"\
 	"|France|Germany|Greece|India|Italy|Japan|Korea|Latin America|Netherlands|Norway|Poland|Portugal"\
 	"|Russia|Scandinavia|Seven|South Africa|Spain|Sweden|Switzerland|Taiwan|Turkey|UK|USA|World"
-#define MATCH_NAME "(.*) \\(((, )?(" REGIONS "))+\\)"
+#define MATCH_TITLE "(.*) \\(((, )?(" REGIONS "))+\\)"
 #define MATCH_REGION "\\((((, )?(" REGIONS "))+)\\)"
 #define MATCH_LANG "\\((\\w+(,\\w+)+)\\)"
 
@@ -42,7 +42,7 @@ int main(int argc, char** argv) {
 	if (argc >= 2) {
 		input_filename = argv[1];
 	} else {
-		error("%s <dat file>\n", argv[0]);
+		info("arg err: input.dat [output.dat]\n");
 		exit(1);
 	}
 	char* output_filename;
@@ -62,95 +62,111 @@ int main(int argc, char** argv) {
 		exit(1);
 	}
 
-	regex_t reg_name;
-	regex_t reg_region;
-	regex_t reg_lang;
-	int status = regcomp(&reg_name, MATCH_NAME, REG_EXTENDED);
-	status = regcomp(&reg_region, MATCH_REGION, REG_EXTENDED);
-	status = regcomp(&reg_lang, MATCH_LANG, REG_EXTENDED);
-	char title[255];
-	char region[100];
-	char languages[100];
+	int status;
+	regex_t reg_title, reg_region, reg_lang;
+	regmatch_t match[2];
+	char str_buffer[255];
 	char title2[255];
 	int progress_position = 0, progress_count = 0;
 
+	status = regcomp(&reg_title, MATCH_TITLE, REG_EXTENDED);
+	status = regcomp(&reg_region, MATCH_REGION, REG_EXTENDED);
+	status = regcomp(&reg_lang, MATCH_LANG, REG_EXTENDED);
+
 	xmlNodePtr root = xmlDocGetRootElement(document);
 	progress_count = xmlChildElementCount(root);
+
+	// to improve the performance, we'll find the game name/region/language
+	// and insert them temporarily in tree
 	for (xmlNodePtr node = root->children; node != NULL; node = node->next) {
-		if (strcmp(node->name, "game") == 0) {
-			processProgress(progress_position++, progress_count);
+		if (strcmp(node->name, "game") != 0) {
+			continue;
+		}
+		char* name = xmlGetProp(node, "name");
 
-			regmatch_t match[2];
+		status = regexec(&reg_title, name, 2, match, 0);
+		if (status == 0) {
+			int len = match[1].rm_eo - match[1].rm_so;
+			strncpy(str_buffer, name + match[1].rm_so, len);
+			str_buffer[len] = 0;
+			xmlSetProp(node, "x_title", str_buffer);
+		} else {
+			info("error: cannot find title for <%s>\n", name);
+			exit(1);
+		}
 
-			char* name = xmlGetProp(node, "name");
+		status = regexec(&reg_region, name, 2, match, 0);
+		if (status == 0) {
+			int len = match[1].rm_eo - match[1].rm_so;
+			strncpy(str_buffer, name + match[1].rm_so, len);
+			str_buffer[len] = 0;
+			xmlSetProp(node, "x_region", str_buffer);
+		} else {
+			info("error: cannot find language for <%s>\n", name);
+			exit(1);
+		}
 
-			status = regexec(&reg_region, name, 2, match, 0);
-			if (status == 0) {
-				int len = match[1].rm_eo - match[1].rm_so;
-				strncpy(region, name + match[1].rm_so, len);
-				region[len] = 0;
-			} else {
-				info("error: cannot find language for <%s>\n", name);
-				exit(1);
-			}
-
-			status = regexec(&reg_lang, name, 2, match, 0);
-			if (status == 0) {
-				int len = match[1].rm_eo - match[1].rm_so;
-				strncpy(languages, name + match[1].rm_so, len);
-				languages[len] = 0;
-
-				char* language = strtok(languages, ",");
-				while (language != NULL) {
-					insertRelease(node, name, region, language);
-					language = strtok(NULL, ",");
-				}
-			} else {
-				insertRelease(node, name, region, NULL);
-			}
-
-
-			char* cloneof = xmlGetProp(node, "cloneof");
-			if (cloneof != NULL) {
-				continue;
-			}
-			status = regexec(&reg_name, name, 2, match, 0);
-			if (status == 0) {
-				int len = match[1].rm_eo - match[1].rm_so;
-				strncpy(title, name + match[1].rm_so, len);
-				title[len] = 0;
-				//error("%s\n", title);
-			}
-
-			for (xmlNodePtr node2 = node->next; node2 != NULL; node2 = node2->next) {
-				if (strcmp(node2->name, "game") == 0) {
-					char* cloneof2 = xmlGetProp(node2, "cloneof");
-					if (cloneof2 != NULL) {
-						continue;
-					}
-					char* name2 = xmlGetProp(node2, "name");
-
-					status = regexec(&reg_name, name2, 2, match, 0);
-					if (status == 0) {
-						int len = match[1].rm_eo - match[1].rm_so;
-						strncpy(title2, name2 + match[1].rm_so, len);
-						title2[len] = 0;
-					}
-
-					if (strcmp(title, title2) == 0) {
-						xmlSetProp(node2, "cloneof", name);
-					}
-				}
-			}
+		status = regexec(&reg_lang, name, 2, match, 0);
+		if (status == 0) {
+			int len = match[1].rm_eo - match[1].rm_so;
+			strncpy(str_buffer, name + match[1].rm_so, len);
+			str_buffer[len] = 0;
+			xmlSetProp(node, "x_lang", str_buffer);
 		}
 	}
 
-//	xmlDocDump(stdout, document);
+	// here we'll use the previous properties to insert releases and clone relations
+	// then we'll delete the temporary properties
+	for (xmlNodePtr node = root->children; node != NULL; node = node->next) {
+		if (strcmp(node->name, "game") != 0) {
+			continue;
+		}
+
+		processProgress(progress_position++, progress_count);
+
+		char* name = xmlGetProp(node, "name");
+		char* title = xmlGetProp(node, "x_title");
+		char* region = xmlGetProp(node, "x_region");
+		char* languages = xmlGetProp(node, "x_lang");
+
+		if (languages != NULL) {
+			char* language = strtok(languages, ",");
+			while (language != NULL) {
+				insertRelease(node, name, region, language);
+				language = strtok(NULL, ",");
+			}
+		} else {
+			insertRelease(node, name, region, NULL);
+		}
+
+		char* cloneof = xmlGetProp(node, "cloneof");
+		if (cloneof == NULL) {
+			for (xmlNodePtr node2 = node->next; node2 != NULL; node2 = node2->next) {
+				if (strcmp(node2->name, "game") != 0) {
+					continue;
+				}
+
+				char* cloneof2 = xmlGetProp(node2, "cloneof");
+				if (cloneof2 != NULL) {
+					continue;
+				}
+
+				char* title2 = xmlGetProp(node2, "x_title");
+				if (strcmp(title, title2) == 0) {
+					xmlSetProp(node2, "cloneof", name);
+				}
+			}
+		}
+
+		xmlUnsetProp(node, "x_title");
+		xmlUnsetProp(node, "x_region");
+		xmlUnsetProp(node, "x_lang");
+	}
 
 	xmlSaveFile(output_filename, document);
 
 	xmlFreeDoc(document);
-	regfree(&reg_name);
+	regfree(&reg_title);
 	regfree(&reg_region);
 	regfree(&reg_lang);
 
